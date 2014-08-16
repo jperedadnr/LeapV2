f88be4abf90f478e5e3549642f034476ec903ec0
jperedadnr <pereda@eii.uva.es>
2014-08-16 15:18:18 +0200

    Improvements in the hand drawing, removing metacarpians from middle and ring finger, and adding virtual bonds between joints. Added the forearm

--- src/leapv2/LeapListener.java
+++ src/leapv2/LeapListener.java
@@ -1,9 +1,12 @@
 package leapv2;
 
+import com.leapmotion.leap.Arm;
 import com.leapmotion.leap.Bone;
 import com.leapmotion.leap.Controller;
 import com.leapmotion.leap.Finger;
+import com.leapmotion.leap.FingerList;
 import com.leapmotion.leap.Frame;
+import com.leapmotion.leap.Hand;
 import com.leapmotion.leap.Listener;
 import com.leapmotion.leap.Screen;
 import java.util.ArrayList;
@@ -22,12 +25,16 @@ public class LeapListener extends Listener {
     // Since we'll be listening to changes only in doneList, we don't need
     // bones collection to be observable too
     private final List<Bone> bones=new ArrayList<>();
+    private final List<Arm> arms=new ArrayList<>();
+    private final List<Pair> joints=new ArrayList<>();
     
     @Override
     public void onFrame(Controller controller) {
         Frame frame = controller.frame();
         doneList.set(false);
         bones.clear();
+        arms.clear();
+        joints.clear();
         if (!frame.hands().isEmpty()) {
             Screen screen = controller.locatedScreens().get(0);
             if (screen != null && screen.isValid()){
@@ -35,19 +42,63 @@ public class LeapListener extends Listener {
                     if(finger.isValid()){
                         for(Bone.Type b : Bone.Type.values()) {
                             Bone bone = finger.bone(b);
-                            bones.add(bone);
+                            if((!finger.type().equals(Finger.Type.TYPE_RING) && 
+                                !finger.type().equals(Finger.Type.TYPE_MIDDLE)) || 
+                                !b.equals(Bone.Type.TYPE_METACARPAL)){
+                                bones.add(bone);
+                            }
                         }
+                    }
+                }
+                for(Hand h: frame.hands()){
+                    if(h.isValid()){
+                        // arm
+                        arms.add(h.arm());
+                        
+                        FingerList fingers = h.fingers();
+                        // joints
+                        if(fingers.get(1).isFinger() && fingers.get(2).isFinger()){
+                            Pair p=new Pair(fingers.get(1).bone(Bone.Type.TYPE_METACARPAL).nextJoint(),
+                                            fingers.get(2).bone(Bone.Type.TYPE_METACARPAL).nextJoint());
+                            joints.add(p);
+                        }
+                        if(fingers.get(2).isFinger() && fingers.get(3).isFinger()){
+                            Pair p=new Pair(fingers.get(2).bone(Bone.Type.TYPE_METACARPAL).nextJoint(),
+                                            fingers.get(3).bone(Bone.Type.TYPE_METACARPAL).nextJoint());
+                            joints.add(p);
+                        }
+                        if(fingers.get(3).isFinger() && fingers.get(4).isFinger()){
+                            Pair p=new Pair(fingers.get(3).bone(Bone.Type.TYPE_METACARPAL).nextJoint(),
+                                            fingers.get(4).bone(Bone.Type.TYPE_METACARPAL).nextJoint());
+                            joints.add(p);
+                        }
+                        if(fingers.get(1).isFinger() && fingers.get(4).isFinger()){
+                            Pair p=new Pair(fingers.get(1).bone(Bone.Type.TYPE_METACARPAL).prevJoint(),
+                                            fingers.get(4).bone(Bone.Type.TYPE_METACARPAL).prevJoint());
+                            joints.add(p);
+                        }        
                     }
                 }
             }
         }
-        doneList.set(!bones.isEmpty());
+        
+        doneList.set(!bones.isEmpty() || !arms.isEmpty());
     }
     
     public List<Bone> getBones(){ 
         // Returns a fresh copy of the bones collection 
         // to avoid concurrent exceptions iterating this list
         return bones.stream().collect(Collectors.toList());
+    }
+    public List<Arm> getArms(){ 
+        // Returns a fresh copy of the arms collection 
+        // to avoid concurrent exceptions iterating this list
+        return arms.stream().collect(Collectors.toList());
+    }
+    public List<Pair> getJoints(){ 
+        // Returns a fresh copy of the joints collection 
+        // to avoid concurrent exceptions iterating this list
+        return joints.stream().collect(Collectors.toList());
     }
     
     public BooleanProperty doneListProperty() { 
--- src/leapv2/LeapV2.java
+++ src/leapv2/LeapV2.java
@@ -1,14 +1,13 @@
 package leapv2;
 
+import com.leapmotion.leap.Arm;
 import com.leapmotion.leap.Bone;
 import com.leapmotion.leap.Controller;
 import com.leapmotion.leap.Vector;
 import java.util.List;
-import java.util.stream.Collectors;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.geometry.Point3D;
-import javafx.scene.Camera;
 import javafx.scene.Group;
 import javafx.scene.PerspectiveCamera;
 import javafx.scene.PointLight;
@@ -24,6 +23,7 @@ import javafx.scene.shape.Cylinder;
 import javafx.scene.shape.DrawMode;
 import javafx.scene.shape.MeshView;
 import javafx.scene.shape.Shape3D;
+import javafx.scene.shape.Sphere;
 import javafx.scene.shape.TriangleMesh;
 import javafx.scene.transform.Rotate;
 import javafx.scene.transform.Translate;
@@ -52,7 +52,8 @@ public class LeapV2 extends Application {
         AnchorPane pane=new AnchorPane();
         
         Scene scene = new Scene(pane, 800, 600, Color.BEIGE);
-        final Camera camera = new PerspectiveCamera();
+        final PerspectiveCamera camera = new PerspectiveCamera();
+        camera.setFieldOfView(90);
         camera.getTransforms().addAll(cameraXRotate,cameraYRotate,cameraPosition);
 
         PhongMaterial material = new PhongMaterial();
@@ -102,12 +103,17 @@ public class LeapV2 extends Application {
         final PhongMaterial materialFinger = new PhongMaterial();
         materialFinger.setDiffuseColor(Color.GOLDENROD);
         materialFinger.setSpecularColor(Color.rgb(50, 50, 50));
+        final PhongMaterial materialArm = new PhongMaterial();
+        materialArm.setDiffuseColor(Color.CORNSILK);
+        materialArm.setSpecularColor(Color.rgb(30, 30, 30));
 
         listener.doneListProperty().addListener((ov,b,b1)->{
             if(b1){
                 
-                // First, get a fresh copy of the bones collection
+                // First, get a fresh copy of the bones, arms & joints collection
                 List<Bone> bones=listener.getBones();
+                List<Arm> arms=listener.getArms();
+                List<Pair> joints=listener.getJoints();
                 
                 Platform.runLater(()->{
                     // Now, on the JavaFX thread
@@ -119,27 +125,96 @@ public class LeapV2 extends Application {
                     // Iterate over the list adding the bones to the scene
                     // If the collection changes there won't be any concurrent exception
                     // as we are iterating over its copy.
-                     bones.stream()
+                    bones.stream()
                         .filter((bone) -> (bone.isValid() && bone.length()>0))
                         .forEach((bone) -> {
                             final Vector p=bone.center();
-                            // create bone as a cylinder and locate it at its center position
+                            // create bone as a vertical cylinder and locate it at its center position
                             Cylinder c=new Cylinder(bone.width()/2,bone.length());
                             c.setMaterial(materialFinger);
-                            c.setTranslateX(200+p.getX());
-                            c.setTranslateY(-p.getY());
-                            c.setTranslateZ(-p.getZ());
                             
-                            // rotate the cylinder towards its direction
+                            // translate and rotate the cylinder towards its direction
                             final Vector v=bone.direction();
                             Vector cross = (new Vector(v.getX(),-v.getY(),-v.getZ())).cross(new Vector(0,-1,0));
                             double ang=(new Vector(v.getX(),-v.getY(),-v.getZ())).angleTo(new Vector(0,-1,0));
-                            c.setRotationAxis(new Point3D(cross.getX(),-cross.getY(),cross.getZ()));
-                            c.setRotate(-Math.toDegrees(ang));
+                            c.getTransforms().addAll(
+                                    new Translate(200+p.getX(),-p.getY(),-p.getZ()),
+                                    new Rotate(-Math.toDegrees(ang), 
+                                        0, 0, 0, 
+                                        new Point3D(cross.getX(),-cross.getY(),cross.getZ()))
+                                    );
                             
                             // add bone to scene
                             root.getChildren().add(c);
-                        });        
+                            
+                            Sphere s=new Sphere(bone.width()/2f*1.2);
+                            s.setMaterial(materialFinger);
+                            s.getTransforms().addAll(
+                                    new Translate(200+p.getX(),-p.getY()+bone.length()/2d,-p.getZ()),
+                                    new Rotate(-Math.toDegrees(ang), 
+                                        0, -bone.length()/2d, 0, 
+                                        new Point3D(cross.getX(),-cross.getY(),cross.getZ())));
+                            
+                            // add bone to scene
+                            root.getChildren().add(s);
+                            
+                            Sphere s2=new Sphere(bone.width()/2f*1.2);
+                            s2.setMaterial(materialFinger);
+                            s2.getTransforms().addAll(
+                                    new Translate(200+p.getX(),-p.getY()-bone.length()/2d,-p.getZ()),
+                                    new Rotate(-Math.toDegrees(ang), 
+                                        0, bone.length()/2d, 0, 
+                                        new Point3D(cross.getX(),-cross.getY(),cross.getZ())));
+                            
+                            // add bone to scene
+                            root.getChildren().add(s2);
+                            
+                        });
+                     
+                    arms.stream()
+                        .filter(arm->arm.isValid())
+                        .forEach(arm->{
+                            final Vector p=arm.center();
+                            // create bone as a cylinder and locate it at its center position
+                            Cylinder c=new Cylinder(arm.width()/2,arm.elbowPosition().minus(arm.wristPosition()).magnitude());
+                            c.setMaterial(materialArm);
+
+                            // rotate the cylinder towards its direction
+                            final Vector v=arm.direction();
+                            Vector cross = (new Vector(v.getX(),-v.getY(),-v.getZ())).cross(new Vector(0,-1,0));
+                            double ang=(new Vector(v.getX(),-v.getY(),-v.getZ())).angleTo(new Vector(0,-1,0));
+                            c.getTransforms().addAll(
+                                    new Translate(200+p.getX(),-p.getY(),-p.getZ()),
+                                    new Rotate(-Math.toDegrees(ang), 
+                                        0, 0, 0, 
+                                        new Point3D(cross.getX(),-cross.getY(),cross.getZ()))
+                                    );
+                            
+                            // add arm to scene
+                            root.getChildren().add(c);
+                        });
+                    
+                    joints.stream()
+                        .forEach(joint->{
+                            double length=joint.getV0().distanceTo(joint.getV1());
+                            Cylinder c=new Cylinder(bones.get(0).width()/4,length);
+                            c.setMaterial(materialArm);
+                            
+                            final Vector p=joint.getCenter();
+                            final Vector v=joint.getDirection();
+                            Vector cross = (new Vector(v.getX(),-v.getY(),-v.getZ())).cross(new Vector(0,-1,0));
+                            double ang=(new Vector(v.getX(),-v.getY(),-v.getZ())).angleTo(new Vector(0,-1,0));
+                            c.getTransforms().addAll(
+                                    new Translate(200+p.getX(),-p.getY(),-p.getZ()),
+                                    new Rotate(-Math.toDegrees(ang), 
+                                        0, 0, 0, 
+                                        new Point3D(cross.getX(),-cross.getY(),cross.getZ()))
+                                    );
+                            
+                                                        
+                            // add joint to scene
+                            root.getChildren().add(c);
+                        });
                 });
             }
         });
